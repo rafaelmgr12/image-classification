4718ed797ae0836fc360a047b29d0624f9b931ae
Rafael R <ribeirorafaelmatheus@gmail.com>
2022-02-21 19:07:04 -0300

    structure ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
 
-function App() {
+import * as mobilenet from "@tensorflow-models/mobilenet";
+
+export default function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+
+  const imageRef = useRef();
+
+  async function loadModel() {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsModelLoading(false);
+    }
+  }
+  function uploadImage(input) {
+    const { files } = input.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageURL(url);
+    } else {
+      setImageURL(null);
+    }
+  }
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  if (isModelLoading) {
+    return <h2> Model is loading</h2>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Identification</h1>
+      <div className="inputholder">
+        <input
+          type="file"
+          accept="image/*"
+          capture="camera"
+          className="uploadInput"
+          onChange={uploadImage}
+        />
+      </div>
+      <div className="mainWrapper">
+        <div className="mainContente">
+          <div className="imageHolder">
+            {imageURL && (
+              <img
+                src={imageURL}
+                alt="Upload Preview"
+                crossOrigin="anonymous"
+                ref={imageRef}
+              />
+            )}
+          </div>
+        </div>
+        {imageURL && <button className="button">Identify Image</button>}
+      </div>
     </div>
   );
 }
-
-export default App;
